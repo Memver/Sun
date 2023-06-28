6384506364199720ad647a55774bda75a973f6d6
Левин Иван <ka_ba@bk.ru>
2023-06-28 12:35:37 +0400

    commit

--- src/main/java/com/sun/hope/AggregatorApplication.java
+++ src/main/java/com/sun/hope/AggregatorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AggregatorApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AggregatorApplication.class, args);
 	}
