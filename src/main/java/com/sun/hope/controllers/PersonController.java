d60c2efd8c721717b051b021c0c1238756b3a79e
Левин Иван <ka_ba@bk.ru>
2023-06-28 12:36:31 +0400

    Was added log

--- src/main/java/com/sun/hope/controllers/PersonController.java
+++ src/main/java/com/sun/hope/controllers/PersonController.java
@@ -2,6 +2,8 @@ package com.sun.hope.controllers;
 
 import com.sun.hope.repositories.PersonRepository;
 import com.sun.hope.repositories.PersonRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +13,11 @@ import java.sql.SQLException;
 public class PersonController {
 
     private final PersonRepository personRepository = new PersonRepositoryImpl();
+    Logger logger = LoggerFactory.getLogger(PersonController.class);
 
     @GetMapping("/person")
     public ResponseEntity<?> personMain() throws SQLException {
+        logger.info("run GET /person");
         return ResponseEntity
                 .status(200)
                 .body(personRepository.findAll());
