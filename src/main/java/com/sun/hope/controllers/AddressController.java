048c1c305685ed25df1ddbb05fb851eb4be10bee
Левин Иван <ka_ba@bk.ru>
2023-06-28 12:36:15 +0400

    Was added log

--- src/main/java/com/sun/hope/controllers/AddressController.java
+++ src/main/java/com/sun/hope/controllers/AddressController.java
@@ -3,6 +3,8 @@ package com.sun.hope.controllers;
 import com.sun.hope.models.Address;
 import com.sun.hope.repositories.AddressRepository;
 import com.sun.hope.repositories.AddressRepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,11 @@ import java.util.List;
 public class AddressController {
 
     private final AddressRepository addressRepository = new AddressRepositoryImpl();
+    Logger logger = LoggerFactory.getLogger(AddressController.class);
 
     @GetMapping("/address")
     public List<Address> getAllAddress() throws SQLException {
+        logger.info("run GET /address");
         return addressRepository.findAll();
     }
 }
